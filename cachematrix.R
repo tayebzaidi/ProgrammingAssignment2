c2574a3788522ff55a4a8d24161e24f22a6b5575
tensortrash <tayebzaidi@gmail.com>
2015-08-17 20:32:11 -0700

    First forked commit, added vector caching for testing, as well as matrixcaching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## An R script with two functions to cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function initializes the matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) i <<- solve
+        getinverse <- function() i
+        list(set = set, get = get, 
+                setinverse = setinverse,
+                getinverse = getinverse)
+        
 }
 
 
-## Write a short comment describing this function
+## Function that returns the inverse of a matrix
+## Expects as input the list of functions returned by
+##
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("Getting cached data")
+                return(i)
+        }
+        data <- x$get() 
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i 
 }
